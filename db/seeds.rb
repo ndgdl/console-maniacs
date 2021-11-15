ce6082491a8b0e4253d0cac7673fa9ebf1ad3799
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-15 22:50:00 +0100

    seed to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+puts 'Cleaning DB...'
+
+Console.destroy_all
+
+puts 'Cleaning done'
+
+
+puts 'Seeding...'
+
+new_console = Console.new(
+  name: ,
+  model: ,
+  content: ,
+  price_per_day: ,
+)
+
+nico = User.find(1)
+jerem = User.find(2)
+
+users = [nico, jerem]
+
+new_console.user = users.sample
+
+new_console.save!
+
+consoles = ["Game Boy",
+ "Tamagotchi",
+ "Nintendo Switch",
+ "PlayStation 3",
+ "PlayStation 4",
+ "PlayStation 5",
+ "Wii",
+ "Xbox",
+ "Xbox 360",
+ "Xbox One",
+ "Xbox Series X"]
+
+i = 1
+
+selected_console = consoles.sample.gsub(' ', '')
+rand(3..5).times do
+  file = URI.open(Faker::LoremFlickr.image(size: "1000x1000", search_terms: ['gaming', 'console', "#{selected_console}"], match_all: true))
+  new_console.photo.attach(io: file, file_name: "#{selected_console}-#{i}.jpeg", content_type: 'image/jpeg')
+  i += 1
+end
+puts 'Seeding done:'
+puts "#{Console.count} created"
