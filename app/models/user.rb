cf4b54ae920a90a9e54284e19a1b077cf4a4e8a8
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-15 15:33:48 +0100

    user model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
+  validates :first_name, :last_name, :phone_number, presence: true
 end
