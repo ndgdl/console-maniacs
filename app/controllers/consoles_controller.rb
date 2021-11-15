d07031d504a09962586a8e31433b63e1fcc4d3de
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-15 18:16:23 +0100

    new and create actions done

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -6,4 +6,24 @@ class ConsolesController < ApplicationController
   def show
     @console = Console.find(params[:id])
   end
+
+  def new
+    @console = Console.new
+  end
+
+  def create
+    @console = Console.new(console_params)
+    @console.user = current_user
+    if @console.save
+      redirect_to console_path(@console)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def console_params
+    params.require(:console).permit(:name, :price_per_day, :content, :model, photos: [])
+  end
 end
