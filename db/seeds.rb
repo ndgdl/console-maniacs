8245ae56213b06f0246a2c3d5045022d9480966a
Alex Vigreux <alex.vigreux@kedgebs.com>
2021-11-17 10:53:46 +0100

    Add two new buttons on homepage and footer in shared files

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,10 +68,8 @@ file = URI.open('https://avatars.githubusercontent.com/u/88536137?v=4')
 nicolas.photo.attach(io: file, filename: 'nicolas.jpg', content_type: 'image/png')
 nicolas.save
 
-
 puts 'Seeding done:'
 puts "#{User.count} users created"
-
 
 puts 'Seeding consoles...'
 
@@ -89,22 +87,22 @@ consoles = ["Game Boy",
 
 count = 1
 rand(5..10).times do
-  puts "Creating #{count} console"
-  new_console_name = consoles.sample
-  new_console = Console.new(
-    name: new_console_name,
-    model: new_console_name.split[0],
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    price_per_day: rand(25.0..75.0).round(2)
-  )
+ puts "Creating #{count} console"
+ new_console_name = consoles.sample
+ new_console = Console.new(
+  name: new_console_name,
+  model: new_console_name.split[0],
+  content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  price_per_day: rand(25.0..75.0).round(2)
+)
 
   new_console.user = User.all.sample
 
   i = 1
   3.times do
-    file = URI.open("https://loremflickr.com/1000/1000/gaming,console,#{new_console_name.gsub(' ', '').downcase}/all")
-    new_console.photos.attach(io: file, filename: "#{new_console_name.gsub(' ', '')}-#{i}.jpg", content_type: 'image/jpg')
-    i += 1
+   file = URI.open("https://loremflickr.com/1000/1000/gaming,console,#{new_console_name.gsub(' ', '').downcase}/all")
+   new_console.photos.attach(io: file, filename: "#{new_console_name.gsub(' ', '')}-#{i}.jpg", content_type: 'image/jpg')
+   i += 1
   end
 
   new_console.save
