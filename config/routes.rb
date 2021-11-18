a85763422ef9b324c7f5bedf7d33952ac0aba1f7
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-18 10:24:29 +0100

    bookings controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
   devise_for :users
   root to: 'pages#home'
 
