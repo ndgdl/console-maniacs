2888a6475cf759d2fae00712102fb79ad8f0d015
Jeremy Blondeau <lebucheronjoe@gmail.com>
2021-11-18 20:18:13 +0100

    update console model

--- app/models/console.rb
+++ app/models/console.rb
@@ -11,4 +11,9 @@ class Console < ApplicationRecord
     self.user.geocode
   end
 
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
