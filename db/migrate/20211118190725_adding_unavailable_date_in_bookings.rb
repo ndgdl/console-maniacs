c697006d3cb158862dde9d97296f437a2116659e
Jeremy Blondeau <lebucheronjoe@gmail.com>
2021-11-18 20:12:32 +0100

    booking inputs

--- db/migrate/20211118190725_adding_unavailable_date_in_bookings.rb
+++ db/migrate/20211118190725_adding_unavailable_date_in_bookings.rb
@@ -3,5 +3,4 @@ class AddingUnavailableDateInBookings < ActiveRecord::Migration[6.0]
     rename_column :bookings, :starting_date, :start_date
     rename_column :bookings, :ending_date, :end_date
   end
-  end
 end
