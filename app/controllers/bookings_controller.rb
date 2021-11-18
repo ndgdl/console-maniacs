d1842c431582e587bd37a5621ba52a1d41c68061
Nicolas de Guilhem de Lataillade <nicolas.dgdl@hotmail.fr>
2021-11-18 11:01:21 +0100

    Bookings controller created, new routes created

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,22 +1,44 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = current_user.bookings
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+
+    if @booking.save
+      redirect_to console_bookings_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking.update(booking_params)
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:bookings).permit(:starting_date, :ending_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'bookings/destroy'
   devise_for :users
   root to: 'pages#home'
 
-  resources :consoles
+  resources :consoles do
+    resources :bookings, except: [:index, :destroy]
+  end
+
+  resources :bookings, only: :destroy
+
+  get '/users/:id/bookings/', to: "bookings#index"
   get '/users/:id/consoles/', to: "pages#owned_consoles", as: :owned_consoles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
