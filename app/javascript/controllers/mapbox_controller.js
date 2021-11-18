6e2288f8364084b37f02bf7db07528ac0726fcbb
ndgdl <88536137+ndgdl@users.noreply.github.com>
2021-11-18 18:29:18 +0100

    Revert "Final mapbox"

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,7 +1,6 @@
 
 import { Controller } from "@hotwired/stimulus";
 import mapboxgl from '!mapbox-gl';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 export default class extends Controller {
   static values = {
@@ -19,23 +18,12 @@ export default class extends Controller {
     console.log(this.element)
     this._addMarkersToMap();
     this._fitMapToMarkers();
-    this.map.addControl(new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-    }));
   }
 
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      const customMarker = document.createElement('div');
-      customMarker.className = 'marker';
-      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
-      customMarker.style.backgroundSize = 'contain';
-      customMarker.style.width = '50px';
-      customMarker.style.height = '50px';
-
-      new mapboxgl.Marker(customMarker)
+      new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map);
