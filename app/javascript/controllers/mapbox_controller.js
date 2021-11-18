0ca064ba90753a52dd33c587515c694b4192c404
Alex Vigreux <alex.vigreux@kedgebs.com>
2021-11-18 15:20:04 +0100

    Marker gaming updated on Mapbox

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -23,7 +23,14 @@ export default class extends Controller {
   _addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window);
-      new mapboxgl.Marker()
+      const customMarker = document.createElement('div');
+      customMarker.className = 'marker';
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
+      customMarker.style.backgroundSize = 'contain';
+      customMarker.style.width = '50px';
+      customMarker.style.height = '50px';
+
+      new mapboxgl.Marker(customMarker)
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map);
