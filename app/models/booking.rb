363421a2ca138ef4aa1f249966a827746c5d6043
Jeremy Blondeau <lebucheronjoe@gmail.com>
2021-11-19 16:02:22 +0100

    fixed date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :console
 
-  # validates :start_date, :end_date, presence: true, availability: true
+  validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
 
   private
